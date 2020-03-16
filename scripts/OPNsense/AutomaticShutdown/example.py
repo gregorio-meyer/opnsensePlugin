7aefc2699cd71481910e20d68b332105c5ef1a31
Gregorio Meyer <meyergregorio@gmail>
2020-03-16 22:28:53 +0100

    sadas

--- scripts/OPNsense/AutomaticShutdown/example.py
+++ scripts/OPNsense/AutomaticShutdown/example.py
@@ -2,4 +2,12 @@
 print("Program starts...")
 import requests
 import json
-print("Done")
+# key + secret from downloaded apikey.txt
+api_key = 'W7meYzZdEndQGBycVONls8cYU8FBGsnMNoirAwAplMtVz8c1g7M7eR89HJcZaGXfT0i+KwcPpfAwBdy2'
+api_secret = 't7BuWrgGciJeMp3hatlofJ4JufoWtDDwHc3XuZGxC28ratSvZzqLmH+yslZB1YbLk0KXJVXdYJGunS0W'
+
+# define the basics, hostname to use and description used to identify our test rule
+rule_description='OPNsense_fw_api_testrule_1'
+remote_uri="https://10.0.0.5"
+
+print("Done");
