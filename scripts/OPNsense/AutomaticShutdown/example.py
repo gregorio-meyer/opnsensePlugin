52e31668751ea62029f211633e81059dd8940193
Gregorio Meyer <meyergregorio@gmail>
2020-03-16 22:30:30 +0100

    Fix

--- scripts/OPNsense/AutomaticShutdown/example.py
+++ scripts/OPNsense/AutomaticShutdown/example.py
@@ -10,4 +10,40 @@ api_secret = 't7BuWrgGciJeMp3hatlofJ4JufoWtDDwHc3XuZGxC28ratSvZzqLmH+yslZB1YbLk0
 rule_description='OPNsense_fw_api_testrule_1'
 remote_uri="https://10.0.0.5"
 
-print("Done");
+# search for rule
+r = requests.get(
+    "%s/api/firewall/filter/searchRule?current=1&rowCount=7&searchPhrase=%s" % (
+        remote_uri, rule_description
+    ),
+    auth=(api_key, api_secret), verify=False
+)
+
+if r.status_code == 200:
+    print("Request succesful")
+    response = json.loads(r.text)
+    if len(response['rows']) == 0:
+        # create a new rule, identified by rule_description allowing traffic from
+        # 192.168.0.0/24 to 10.0.0.0/24 using TCP protocol
+        data = {"rule" :
+                    {
+                    "description": rule_description,
+                    "source_net": "192.168.0.0/24",
+                    "protocol": "TCP",
+                    "destination_net": "10.0.0.0/24"
+                    }
+                }
+        r = requests.post(
+            "%s/api/firewall/filter/addRule" % remote_uri, auth=(api_key, api_secret), verify=False, json=data
+        )
+        if r.status_code == 200:
+            print("created : %s" % json.loads(r.text)['uuid'])
+        else:
+            print("error : %s" % r.text)
+
+    else:
+        print("Request failed")
+        for row in response['rows']:
+            print ("found uuid %s" % row['uuid'])
+else:
+    print("Request failed!")
+    exit(0)
