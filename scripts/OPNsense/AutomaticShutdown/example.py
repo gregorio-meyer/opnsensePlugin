01a8a6787e04693b972b76181e3ef7e573a6fb4c
Gregorio Meyer <meyergregorio@gmail>
2020-03-16 22:26:18 +0100

    test

--- scripts/OPNsense/AutomaticShutdown/example.py
+++ scripts/OPNsense/AutomaticShutdown/example.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3.7
+print("Program starts...")
 import requests
 import json
-print("Program starts...")
 # key + secret from downloaded apikey.txt
 api_key = 'W7meYzZdEndQGBycVONls8cYU8FBGsnMNoirAwAplMtVz8c1g7M7eR89HJcZaGXfT0i+KwcPpfAwBdy2'
 api_secret = 't7BuWrgGciJeMp3hatlofJ4JufoWtDDwHc3XuZGxC28ratSvZzqLmH+yslZB1YbLk0KXJVXdYJGunS0W'
