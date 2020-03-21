3812610b6640e1e7830e19c1f4b993c0caf135c6
Gregorio Meyer <meyergregorio@gmail>
2020-03-21 17:29:06 +0100

    fix

--- mvc/app/controllers/OPNsense/TrafficBlocker/Api/ServiceController.php
+++ mvc/app/controllers/OPNsense/TrafficBlocker/Api/ServiceController.php
@@ -24,22 +24,38 @@ class ServiceController extends ApiControllerBase
     /**
      * reconfigure TrafficBlocker
      */
+    public function startAction()
+    {
+        if ($this->request->isPost()) {
+            $backend = new Backend();
+            $mdl = new TrafficBlocker();
+            $result['message'] = $mdl->getNodes();
+            $ip = strval($result['message']['general']['Ip']);
+            $bckresult = trim($backend->configdRun('trafficblocker start ' . $ip));
+            if ($bckresult !== null) {
+                return $bckresult;
+            } else {
+                $status = "Start error";
+            }
+        }
+        return array("message" => $status);
+    }
     public function reloadAction()
     {
-   //     $status = "failed";
-/*         if ($this->request->isPost()) {
+        //     $status = "failed";
+        if ($this->request->isPost()) {
             $backend = new Backend();
             $bckresult = trim($backend->configdRun('template reload OPNsense/TrafficBlocker'));
             if ($bckresult == "OK") {
                 $mdl = new TrafficBlocker();
-             //   $result['message'] = $mdl->getNodes();
+                $result['message'] = $mdl->getNodes();
                 $ip = strval($result['message']['general']['Ip']);
-                $backend->configdRun('trafficblocker start '.$ip);
+                $backend->configdRun('trafficblocker start ' . $ip);
                 $status = "ok";
             }
         }
-        return array("message" => $status); */
-       // return $result;
+        return array("message" => $status);
+        // return $result;
     }
     public function statusAction()
     {
