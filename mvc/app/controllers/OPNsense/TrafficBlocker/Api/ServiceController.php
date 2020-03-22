8dfffed30f186faf74cf6f7646b5a893f2d7c427
Gregorio Meyer <meyergregorio@gmail>
2020-03-22 13:45:26 +0100

    fix

--- mvc/app/controllers/OPNsense/TrafficBlocker/Api/ServiceController.php
+++ mvc/app/controllers/OPNsense/TrafficBlocker/Api/ServiceController.php
@@ -69,6 +69,6 @@ class ServiceController extends ApiControllerBase
             }
             
         }
-        return array("message" => "Status: unable to run config action result: ".$bckresult." enabled:".$enabled);
+        return array("message" => "Result: ".$bckresult." enabled:".$enabled);
     }
 }
