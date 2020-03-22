da5ef986d877186cc14d983a637c10553eaa9c77
Gregorio Meyer <meyergregorio@gmail>
2020-03-22 17:58:45 +0100

    fix

--- scripts/OPNsense/AutomaticShutdown/getStatus.py
+++ scripts/OPNsense/AutomaticShutdown/getStatus.py
@@ -2,16 +2,11 @@
 import requests
 import json
 
-print("Program starts...")
-# key + secret from downloaded apikey.txt
+# key + secret 
 api_key = 'W7meYzZdEndQGBycVONls8cYU8FBGsnMNoirAwAplMtVz8c1g7M7eR89HJcZaGXfT0i+KwcPpfAwBdy2'
 api_secret = 't7BuWrgGciJeMp3hatlofJ4JufoWtDDwHc3XuZGxC28ratSvZzqLmH+yslZB1YbLk0KXJVXdYJGunS0W'
-
-# define the basics, hostname to use and description used to identify our test rule
 url = "http://10.0.0.5/api/automaticshutdown/settings/get"
-# add wake on lan
-# create a new rule, identified by rule_description allowing traffic from
-# 192.168.0.0/24 to 10.0.0.0/24 using TCP protocol
+
 r = requests.get(url, auth=(api_key, api_secret),
                   verify=False)
 if r.status_code == 200:
