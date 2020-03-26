3245a93fe9cbe8b3df1eabcf016e499b6dffaee1
Gregorio Meyer <meyergregorio@gmail>
2020-03-26 18:39:05 +0100

    a

--- scripts/OPNsense/TrafficBlocker/stopBlockWifi.py
+++ scripts/OPNsense/TrafficBlocker/stopBlockWifi.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3.7
-import sys
-from subprocess import check_call
+#import sys
+#from subprocess import check_call
 print("Stopping traffic blocker..")
-script = "blockWifiIp.py"
+#script = "blockWifiIp.py"
 
 
-check_call(["pkill", "-9", "-f", script])
+#check_call(["pkill", "-9", "-f", script])
