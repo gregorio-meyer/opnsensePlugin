e18317f6ad77fbe5ef6aef81ebade576cef209cd
Gregorio Meyer <meyergregorio@gmail>
2020-03-27 21:28:45 +0100

    a

--- scripts/OPNsense/TrafficBlocker/blockWifiIp.py
+++ scripts/OPNsense/TrafficBlocker/blockWifiIp.py
@@ -113,12 +113,8 @@ def blockTraffic(lock):
 
 
 def checkIftop(ip):
-<<<<<<< HEAD
-    result = os.system("iftop -i em1 -t -s 10 >>log.txt")
-=======
     result = os.system("iftop -i em1 -t -s 10")
     connected = False
->>>>>>> 60e048a57481bee92aa0d3de5910d98b884f9b57
     #parse result and returns a report
     if str.isdigit(result):
         print(result)
