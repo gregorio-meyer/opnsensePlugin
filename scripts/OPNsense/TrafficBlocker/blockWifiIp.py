1b25f369a70f1305dfa2d01870fe70cb2dfc4bb3
Gregorio Meyer <meyergregorio@gmail>
2020-03-28 13:11:36 +0100

    a

--- scripts/OPNsense/TrafficBlocker/blockWifiIp.py
+++ scripts/OPNsense/TrafficBlocker/blockWifiIp.py
@@ -127,7 +127,7 @@ def checkIftop(ip):
     #     print('Return code: ' + str(e.returncode))
     # # a byte object is returned
     # result = p.decode("utf-8")
-    os.system(str(result) +"> log.txt")
+    os.system("echo "+str(result) +"> log.txt")
     #print("Length: ", len(result))
     #print(result)
     # exit(0)6
