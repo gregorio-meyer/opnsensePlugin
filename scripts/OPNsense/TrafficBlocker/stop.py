6e2b81f868c5956de45183395e74c7a405360fcc
Gregorio Meyer <meyergregorio@gmail>
2020-03-30 16:25:22 +0200

    l

--- scripts/OPNsense/TrafficBlocker/stop.py
+++ scripts/OPNsense/TrafficBlocker/stop.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.7
 from subprocess import check_call
+from blockWifiIp import blockTraffic
 import sys
 def stop():
     print("Stopping traffic blocker..")
@@ -9,8 +10,9 @@ def stop():
     except Exception as e:
         print("Failed:%s" % e)
 
-
+#it should also enable connection
 try:
+    blockTraffic(False)
     stop()
 except Exception as e:
     print(e)
