8a8ab6e4562f7d055a3e4ad31eae80d74d66492e
Gregorio Meyer <meyergregorio@gmail>
2020-03-30 16:56:08 +0200

    a

--- scripts/OPNsense/TrafficBlocker/main.py
+++ scripts/OPNsense/TrafficBlocker/main.py
@@ -9,14 +9,14 @@ def run():
     #run it for 10 seconds
     #start blockWifi.start() as a process 
     print("Checking host connections") 
-    p = multiprocessing.Process(target=start,name="Start", args=(10,))
+    p = multiprocessing.Process(target=start,name="Start")
     p.start()
     time.sleep(10)
 #stop and update the view
     p.terminate()
     p.join()
     print("Check ended")
-    
+
     #run it every 10 seconds
     threading.Timer(10, run).start()
 
